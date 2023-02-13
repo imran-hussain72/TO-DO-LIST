f477829ec55887e9a238d44564e88750e99250f1
Sayyed Imran Hussain <115886121+imran-hussain72@users.noreply.github.com>
2023-02-13 20:44:17 +0530

    Add files via upload

---
+++ To-Do-List/script.js
@@ -0,0 +1,54 @@
+const item = document.querySelector('#item')
+const toDoBox = document.querySelector('#to-do-box')
+
+
+item.addEventListener(
+    "keyup",
+    function(event){
+        if(event.key == "Enter"){
+            addToDo(this.value);
+            this.valuen = ""
+        }
+    }
+)
+
+
+
+const addToDo = (item)=>{
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+       ${item}
+      <i class="fas fa-times"></i>
+    `;
+
+    listItem.addEventListener(
+        "click",
+        function(){
+            this.classList.toggle("done")
+        }
+    )
+
+    listItem.querySelector("i").addEventListener(
+        'click',
+        function(){
+            listItem.remove()
+        }
+    )
+    toDoBox.appendChild(listItem)
+}
+
+
+
+
+
+
+
+// const addToDo = (item) => {
+//     const listItem = document.createElement("li")
+//     listItem.innerHTML = `
+//        ${item}
+//     <i class="fas fa-times"></i>
+//     `;
+
+//     toDoBox.appendChild(listItem)
+// }
